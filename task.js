e10de1d847821b6a24a9e2779b8b9fbd391d3dd2
Roman <Roman.Vengl1@gmail.com>
2019-11-26 17:39:11 +0200

    added task 1,3

--- task.js
+++ task.js
@@ -1,8 +1,41 @@
+const ChangeTheme = {
+  isActive: false,
+
+  start() {
+    if (this.isActive) {
+      return;
+    } else {
+      this.isActive = true;
+      this.colorsThemeId = setInterval(() => {
+        const randomIntegerFromInterval = (min, max) => {
+          return Math.floor(Math.random() * (max - min + 1) + min);
+        };
+        const result = randomIntegerFromInterval(6, -1);
+        refs.body.style.backgroundColor = colors[result];
+      }, 1000);
+    }
+  },
+
+  stop() {
+    clearInterval(this.colorsThemeId);
+    this.isActive = false;
+  }
+};
+
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548"
 ];
+
+const refs = {
+  BtnStart: document.querySelector('[data-action="start"]'),
+  BtnStop: document.querySelector('[data-action="stop"]'),
+  body: document.body
+};
+
+refs.BtnStart.addEventListener("click", ChangeTheme.start.bind(ChangeTheme));
+refs.BtnStop.addEventListener("click", ChangeTheme.stop.bind(ChangeTheme));
